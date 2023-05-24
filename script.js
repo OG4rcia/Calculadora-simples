0aec152510adee7763aa5e203ded4416603a59a6
Gabriel Garcia de Carvalho <125706611+OG4rcia@users.noreply.github.com>
2023-05-24 17:12:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+number_1 = 0;  
+
+function num1 () {
+    if(document.getElementById('exibicao').value == "") {
+        document.getElementById('exibicao').value = 1;
+    } else {
+        document.getElementById('exibicao').value = document.getElementById('exibicao').value + "+" + 1;
+    }
+}
+function num2 () {
+    if(document.getElementById('exibicao').value == "") {
+        document.getElementById('exibicao').value = 2;
+    }
+}
+function num3 () {
+    console.log(3)
+    document.getElementById('exibicao').value = 2;
+}
+function num4 () {
+    console.log(4)
+    document.getElementById('exibicao').value = 2;
+}
+function num5 () {
+    console.log(5)
+    document.getElementById('exibicao').value = 2;
+}
+function num6 () {
+    console.log(6)
+    document.getElementById('exibicao').value = 2;
+}
+function num7 () {
+    console.log(7)
+    document.getElementById('exibicao').value = 2;
+}
+function num8 () {
+    console.log(8)
+    document.getElementById('exibicao').value = 2;
+}
+function num9 () {
+    console.log(9)
+    document.getElementById('exibicao').value = 2;
+}
+function num0 () {
+    console.log(0)
+    document.getElementById('exibicao').value = 2;
+}
